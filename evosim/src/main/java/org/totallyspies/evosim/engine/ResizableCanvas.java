4af88b9b2b0eb9c59b479705fb8b1d5ac5bc1c97
EnYiHou <enyi.hou@gmail.com>
2023-02-27 11:10:12 -0500

    Cleaned the code
    Cleaned some codes

--- evosim/src/main/java/org/totallyspies/evosim/engine/ResizableCanvas.java
+++ evosim/src/main/java/org/totallyspies/evosim/engine/ResizableCanvas.java
@@ -1,33 +1,33 @@
 package org.totallyspies.evosim.engine;
-
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
-
 /**
- * This class consists of a StackPane containing the Canvas in which the simulation is rendered.
+ * This class consists of a StackPane containing the
+ * Canvas in which the simulation is rendered.
  * The Canvas will be automatically resized when this is resized.
  */
 public class ResizableCanvas extends StackPane {
-
-    final private Canvas canvas = new Canvas();
-
     /**
-     * Creates a new ResizableCanvas
+     * The canvas in which the simulation is rendered.
+     */
+    private final Canvas canvas = new Canvas();
+    /**
+     * Creates a new ResizableCanvas.
      */
     public ResizableCanvas() {
         this.getChildren().add(canvas);
         canvas.widthProperty().bind(this.widthProperty());
         canvas.heightProperty().bind(this.heightProperty());
     }
-
     /**
-     * Returns the graphicContext2D of the canvas. Used to perform drawing operations on the canvas.
-     * @return the graphicContext2D of the canvas
+     * Returns the graphicContext2D of the canvas.
+     * Used to perform drawing operations on the canvas.
+     *
+     * @return the graphicContext2D of the canvas.
      */
     public GraphicsContext getGraphicsContext2D() {
         return canvas.getGraphicsContext2D();
     }
-
-
 }
+
