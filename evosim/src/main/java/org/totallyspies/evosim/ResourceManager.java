63454f29e44442ce88a3fcc7994d785abf9b2ff6
niakouu <edddl.assas@gmail.com>
2023-03-10 10:46:00 -0500

    Add ResourceManager class.

--- evosim/src/main/java/org/totallyspies/evosim/ResourceManager.java
+++ evosim/src/main/java/org/totallyspies/evosim/ResourceManager.java
@@ -1,16 +1,21 @@
 package org.totallyspies.evosim;
 
 /**
- * A ResourceManager class privide
+ * A ResourceManager class provides resources definition used in
+ * this application.
  *
  * @author edeli
  */
 public final class ResourceManager {
+
+  /**
+   * Private constructor to make static class.
+   */
   private ResourceManager() {
   }
 
   private static final String RESOURCE_FOLDER = "org/totallyspies/evosim/";
-  private static final String FXML_FOLDER = RESOURCE_FOLDER + "/fxml/";
+  private static final String FXML_FOLDER = "/" + RESOURCE_FOLDER + "fxml/";
   private static final String IMAGE_FOLDER = RESOURCE_FOLDER + "images/";
   private static final String SOUND_FOLDER = RESOURCE_FOLDER + "sounds/";
 
