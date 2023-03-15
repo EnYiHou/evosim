4661004c7802103bc0a3b3fa8571a1a3d0f75c33
ptrstr <ptrstr@protonmail.com>
2023-03-15 01:11:20 -0400

    Fix requested changes

--- evosim/src/main/java/org/totallyspies/evosim/ResourceManager.java
+++ evosim/src/main/java/org/totallyspies/evosim/ResourceManager.java
@@ -14,12 +14,12 @@ public final class ResourceManager {
   private ResourceManager() {
   }
 
-  private static final String RESOURCE_FOLDER = "org/totallyspies/evosim/";
-  private static final String FXML_FOLDER = "/" + RESOURCE_FOLDER + "fxml/";
-  private static final String IMAGE_FOLDER = RESOURCE_FOLDER + "images/";
-  private static final String SOUND_FOLDER = RESOURCE_FOLDER + "sounds/";
+  private static final String RESOURCE_FOLDER = "/org/totallyspies/evosim";
+  private static final String FXML_FOLDER = RESOURCE_FOLDER + "/fxml";
+  private static final String IMAGE_FOLDER = RESOURCE_FOLDER + "/images";
+  private static final String SOUND_FOLDER = RESOURCE_FOLDER + "/sounds";
 
   // FXML files
-  public static final String FXML_MAIN_VIEW = FXML_FOLDER + "main.fxml";
+  public static final String FXML_MAIN_VIEW = FXML_FOLDER + "/main.fxml";
 
 }
