ef1f33f133ac5ea1939a65ba059153973d4161da
mattlep11 <103292287+mattlep11@users.noreply.github.com>
2023-03-16 14:39:43 -0400

    Feature/1/neural network compute (#52)
    * Implemented the Neuron class
    * Implemented the network constructor (populate w/ weights & neurons)
    * Implemented Neural Network computations
    * Added changes for pr.
    * Fixed linter issues

---
+++ evosim/src/main/java/org/totallyspies/evosim/neuralnetwork/Neuron.java
@@ -0,0 +1,53 @@
+package org.totallyspies.evosim.neuralnetwork;
+
+import org.totallyspies.evosim.utils.Rng;
+
+/**
+ * A Neuron is a single node of the greater Neural Network system storing a
+ * double valued bias. Each layer of the network is made up of many Neuron
+ * objects.
+ *
+ * @author Matthew
+ */
+public final class Neuron {
+
+    /**
+     * A double value to shift the results neural network's activation.
+     */
+    private double bias;
+
+    /**
+     * The upper bound for generating random biases.
+     */
+    private static final double BIAS_MAX = 1d;
+
+    /**
+     * The lower bound for generating random biases.
+     */
+    private static final double BIAS_MIN = 0d;
+
+    /**
+     * Creates a Neuron with a randomly generated bias.
+     */
+    public Neuron() {
+        this.bias = Rng.RNG.nextDouble(BIAS_MIN, BIAS_MAX);
+    }
+
+    /**
+     * Creates a Neuron with a predefined bias.
+     *
+     * @param neuronBias the bias used to shift the activation function
+     */
+    public Neuron(final double neuronBias) {
+        this.bias = neuronBias;
+    }
+
+    public double getBias() {
+        return this.bias;
+    }
+
+    public void setBias(final double newBias) {
+        this.bias = newBias;
+    }
+
+}
