9cbbc3a25c27562224e1212a760fa46d314e9d1a
EnYiHou <enyi.hou@gmail.com>
2023-03-17 16:57:02 -0400

    Implemented sensors adjustment
    implemented a method that adjust the sensor's positions according to the entity's position and angle.

--- evosim/src/main/java/org/totallyspies/evosim/entity/Prey.java
+++ evosim/src/main/java/org/totallyspies/evosim/entity/Prey.java
@@ -2,18 +2,27 @@ package org.totallyspies.evosim.entity;
 
 public class Prey extends Entity {
 
-    public final static double ANGLE_BETWEEN_SENSORS = 300.0/Entity.SENSORS_COUNT;
+    public final static double VIEW_ANGLE = 300.0d;
+    public final static double ANGLE_BETWEEN_SENSORS = Prey.VIEW_ANGLE/Entity.SENSORS_COUNT;
     public Prey(double speed) {
-        super(speed, Prey.ANGLE_BETWEEN_SENSORS);
+        super(speed);
     }
 
     @Override
-    public void move(double distance) {
+    public void onUpdate() {
 
     }
 
     @Override
-    public void onUpdate(int x, float[] fl) {
-
+    public void adjustSensors() {
+        for (int i = 0; i < Entity.SENSORS_COUNT; i++) {
+            double angle = this.rotationAngleInRadians
+                    + Math.toRadians(- Prey.VIEW_ANGLE / 2
+                    + Prey.ANGLE_BETWEEN_SENSORS * i);
+            this.sensors[i][0] = this.positionX;
+            this.sensors[i][1] = this.positionY;
+            this.sensors[i][2] = this.positionX + Math.cos(angle) * Entity.SENSORS_LENGTH;
+            this.sensors[i][3] = this.positionY + Math.sin(angle) * Entity.SENSORS_LENGTH;
+        }
     }
 }
