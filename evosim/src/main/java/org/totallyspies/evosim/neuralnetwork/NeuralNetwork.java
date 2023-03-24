93297878768003499b8e306c3568573ebd07b307
ptrstr <ptrstr@protonmail.com>
2023-03-24 16:40:11 -0400

    Cleaned up API for neural network (#56)
    * Neuron.java changed.
    
    * NeuralNetwork optimized constructor
    
    * Migrated to clean api
    
    * Test of the Neural Network passed
    
    * Comments changed
    
    * Comments changed
    
    ---------
    
    Co-authored-by: niakouu <edelina.alieva@gmail.com>

--- evosim/src/main/java/org/totallyspies/evosim/neuralnetwork/NeuralNetwork.java
+++ evosim/src/main/java/org/totallyspies/evosim/neuralnetwork/NeuralNetwork.java
@@ -1,9 +1,11 @@
 package org.totallyspies.evosim.neuralnetwork;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.function.Function;
-
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import org.totallyspies.evosim.math.Formulas;
 import org.totallyspies.evosim.utils.Rng;
 
@@ -17,215 +19,119 @@ import org.totallyspies.evosim.utils.Rng;
  * list of weights. The final layer's values (output layer) determines what the
  * Entity's decision will be.
  *
- * @author Matthew
+ * @author Matthew Edelina
  */
 public class NeuralNetwork {
 
-    /**
-     * A list containing each list of Neurons, which each represent a layer.
-     */
-    private List<List<Neuron>> neurons;
+  /**
+   * A list containing each list of Neurons, which each represent a layer.
+   */
+  private List<List<Neuron>> neuronLayers;
 
-    /**
-     * A 3D list containing all the weights of the network.
-     * <p>
-     * Each neuron of the network has a weight attached to every other neuron
-     * from the preceding layer. The first inner list selects which layer and
-     * the second inner list selects which neuron of that layer to look at.
-     * </p>
-     */
-    private List<List<List<Double>>> weights;
+  /**
+   * Constructs a Neural Network with a set number of layers with specific
+   * sizes.
+   *
+   * @param layerSizes a list of integers representing the size of each layer.
+   *                   the number of elements in the list is the number of
+   *                   layers.
+   */
+  public NeuralNetwork(final List<Integer> layerSizes) {
+    this.neuronLayers = new ArrayList<>();
 
-    /**
-     * The activation function randomly selected.
-     */
-    private Function<Double, Double> activationFunction;
+    Function<Double, Double> activationFunction =
+        Formulas
+            .ACTIVATION_FUNCTIONS
+            .get(Rng.RNG.nextInt(0, Formulas.ACTIVATION_FUNCTIONS.size()));
 
-    /**
-     * An array with the same size as the greatest neuron layer used to store
-     * the result of all computations.
-     * <p>
-     * The final decision of the network will
-     * be stored within the length of indices equal to the size of the output
-     * layer.
-     * </p>
-     */
-    private double[] computations;
+    // populate neural network
+    int lastSize = 0;
+    for (int curSize : layerSizes) { // layer number
+      int finalLastSize = lastSize;
 
-    /**
-     * The upper bound used to generate random weights.
-     */
-    private static final double WEIGHT_MAX = 0.99d;
+      this.neuronLayers.add(
+          Stream
+              .generate(() -> finalLastSize == 0
+                  ? new Neuron(1, activationFunction, false)
+                  : new Neuron(finalLastSize, activationFunction, true))
+              .limit(curSize)
+              .toList()
+      );
 
-    /**
-     * The lower bound used to generate random weights.
-     */
-    private static final double WEIGHT_MIN = -0.99d;
+      lastSize = curSize;
+    }
+  }
 
-    /**
-     * Constructs a Neural Network with a set number of layers with specific
-     * sizes.
-     *
-     * @param sizes a list of integers representing the size of each layer. the
-     *              number of elements in the list is the number of layers.
-     */
-    public NeuralNetwork(final List<Integer> sizes) {
-        this.neurons = new ArrayList<>();
-        this.weights = new ArrayList<>();
-        this.activationFunction = Formulas.ACTIVATION_FUNCTIONS.get(
-                Rng.RNG.nextInt(0, Formulas.ACTIVATION_FUNCTIONS.size()));
-        int maxLayerIndex = 0;
+  /**
+   * Create a copy of this Neural Network.
+   *
+   * @return a copy of the Neural Network with similar variables
+   */
+  public NeuralNetwork copy() {
 
-        // populate neural network
-        for (int i = 0; i < sizes.size(); i++) { // layer number
-            neurons.add(new ArrayList<>());
-            for (int j = 0; j < sizes.get(i); j++) { // layer size
-                neurons.get(i).add(new Neuron());
-            }
+    return null;
+  }
 
-            if (neurons.get(i).size() > neurons.get(maxLayerIndex).size()) {
-                maxLayerIndex = i;
-            }
-        }
+  /**
+   * Calculates the network's final decision.
+   *
+   * @param inputs the original inputs from the Entity sensors
+   * @return a List containing the final computations. the only values of
+   * importance are the elements within the length equal to the number of
+   * neurons in the output layer.
+   */
+  public List<Double> calcNetworkDecision(final List<Double> inputs) {
+    Iterator<List<Neuron>> iterator = this.neuronLayers.iterator();
+    List<Neuron> firstLayer = iterator.next();
 
-        // create an array with the size of the largest neuron layer
-        this.computations = new double[this.neurons.get(maxLayerIndex).size()];
-
-        /*populate weights:
-            within each weight layer, there are as many weight lists as
-            neurons on the output (right) side, and within each list there are
-            as many weights as neurons on the input (left) side.*/
-        for (int i = 0; i < sizes.size() - 1; i++) {
-            weights.add(new ArrayList<>());
-            int outputNeuronNum = sizes.get(i + 1);
-            int inputNeuronNum = sizes.get(i);
-
-            for (int j = 0; j < outputNeuronNum; j++) {
-                weights.get(i).add(new ArrayList<>());
-                for (int k = 0; k < inputNeuronNum; k++) {
-                    weights.get(i).get(j).add(
-                            Rng.RNG.nextDouble(WEIGHT_MIN, WEIGHT_MAX));
-                }
-            }
-        }
+    if (inputs.size() != firstLayer.size()) {
+      throw new ArrayIndexOutOfBoundsException();
     }
 
-    /**
-     * Calculates the network's final decision.
-     * <p>
-     * This method calls {@link #computeLayer(double[], java.util.List,
-     * java.util.List, boolean) computeLayer} and {@link #passForwards(double[],
-     * double[]) passForwards} to calculate the dot products between each weight
-     * layer and their associated neuron layer. After the first layer, the
-     * inputs are activated via the network's assigned activation function
-     * before computation. Afterwards, they're multiplied by the neurons bias.
-     * The array of computations is returned on completion.
-     * </p>
-     *
-     * @param inputs   the original inputs from the Entity sensors
-     * @return an array containing the final computations. the only values of
-     * importance are the elements within the length equal to the number of
-     * neurons in the output layer.
-     */
-    public double[] calcNetworkDecision(final List<Double> inputs) {
-        boolean isFirstLayer = true;
+    List<Double> outputs = IntStream
+        .range(0, inputs.size())
+        .mapToDouble(i -> firstLayer.get(i).feed(List.of(inputs.get(i))))
+        .boxed()
+        .toList();
 
-        // calculate decision for each layer
-        for (int i = 0; i < this.weights.size(); i++) {
-            if (isFirstLayer) {
-                passForwards(computeLayer(
-                                inputs.stream().mapToDouble(x -> x).toArray(),
-                                this.weights.get(i),
-                                this.neurons.get(i + 1),
-                                isFirstLayer),
-                        this.computations);
-                isFirstLayer = false;
-            } else {
-                passForwards(computeLayer(this.computations,
-                                this.weights.get(i),
-                                this.neurons.get(i + 1),
-                                isFirstLayer),
-                        this.computations);
-            }
-        }
-        return this.computations;
+    System.out.println(outputs);
+
+    while (iterator.hasNext()) {
+      List<Double> latestOutputs = outputs;
+      outputs = iterator.next()
+          .stream()
+          .map(neuron -> neuron.feed(latestOutputs))
+          .toList();
+      System.out.println(outputs);
     }
 
-    /**
-     * Computes the resultant layer of the neural network.
-     * <p>
-     * This computes the dot products between the input values and the weights
-     * for one layer of the neural network. If the layer is not the first layer,
-     * each value must be activated using the network's activation function
-     * before computation and must be multiplied by the neuron's bias after.
-     * Returns the array of dot product results at the end.
-     * </p>
-     *
-     * @param inputs       an array of input values, either the inputs from the
-     *                     sensors or results from previous layers
-     * @param weightsList  the layer of weights between the two neuron layers
-     * @param neuronsList  the target layer of neurons
-     * @param isFirstLayer a boolean, true if it's the first layer, else false
-     * @return the array of dot product results
-     */
-    private double[] computeLayer(final double[] inputs,
-                                  final List<List<Double>> weightsList,
-                                  final List<Neuron> neuronsList,
-                                  final boolean isFirstLayer) {
-        double[] dotProducts = new double[weightsList.size()];
+    return outputs;
+  }
 
-        // activate each value before computation
-        if (!isFirstLayer) {
-            for (int i = 0; i < neuronsList.size(); i++) {
-                inputs[i] = this.activationFunction.apply(inputs[i]);
-            }
-        }
+  /**
+   * Clones and mutates the current neural network for another entity.
+   * <p>
+   * The neural network is copied for use of new entity. To mutate it, the
+   * values of all the weights and biases are regenerated based on a set
+   * mutation rate.
+   * </p>
+   *
+   * @return the mutated brain
+   */
+  private NeuralNetwork cloneAndMutate() {
+    return null;
+  }
 
-        // compute dot product between inputs & weights for each target neuron
-        for (int i = 0; i < weightsList.size(); i++) {
-            double dotProduct = 0;
-            for (int j = 0; j < weightsList.get(i).size(); j++) {
-                dotProduct += inputs[j] * weightsList.get(i).get(j);
-            }
-            dotProducts[i] = dotProduct;
-        }
+  /**
+   * Test the Neural Network.
+   * @param args
+   */
+  public static void main(final String[] args) {
+    NeuralNetwork neuralNetwork = new NeuralNetwork(List.of(2, 2, 1));
+    System.out.println(neuralNetwork.calcNetworkDecision(
+        List.of(4d, 5d)));
 
-        // multiply each result by the bias
-        if (!isFirstLayer) {
-            for (int i = 0; i < neuronsList.size(); i++) {
-                dotProducts[i] += neuronsList.get(i).getBias();
-            }
-        }
-
-        return dotProducts;
-    }
-
-    /**
-     * Passes forwards the results from the layer computation to a destination
-     * array.
-     * <p>
-     * This overwrites values written in the destination array but does not
-     * modify any values past the length of the source array.
-     * </p>
-     *
-     * @param src  array containing results from layer computation
-     * @param dest target array to copy values into
-     */
-    private void passForwards(final double[] src, final double[] dest) {
-        System.arraycopy(src, 0, dest, 0, src.length);
-    }
-
-    /**
-     * Clones and mutates the current neural network for another entity.
-     * <p>
-     * The neural network is copied for use of new entity. To mutate it, the
-     * values of all the weights and biases are regenerated based on a set
-     * mutation rate.
-     * </p>
-     * @return  the mutated brain
-     */
-    private NeuralNetwork cloneAndMutate() {
-        return null;
-    }
+    System.out.println(neuralNetwork.neuronLayers);
+  }
 
 }
