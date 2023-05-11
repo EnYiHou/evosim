2c9d2ab4c3d5b11c64c250ee5ce76ce0c3a79bc9
niakouu <edelina.alieva@gmail.com>
2023-05-10 22:04:02 -0400

    Removed constructor due to a review

--- evosim/src/main/java/org/totallyspies/evosim/utils/EvosimException.java
+++ evosim/src/main/java/org/totallyspies/evosim/utils/EvosimException.java
@@ -3,13 +3,6 @@ package org.totallyspies.evosim.utils;
 public class EvosimException extends Exception {
 
     /**
-     * If an unknown error occurs.
-     */
-    public EvosimException() {
-        super("JSON File Invalid, Try some other JSON File.");
-    }
-
-    /**
      * If an known error occurs.
      * @param message
      */
