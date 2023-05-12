2b0eea5a487973b82d67fc88315474e92d75d4b7
ptrstr <ptrstr@protonmail.com>
2023-05-12 19:42:28 -0400

    Move to Java 19. Fix resource fetching

--- evosim/src/main/java/org/totallyspies/evosim/fxml/WelcomeController.java
+++ evosim/src/main/java/org/totallyspies/evosim/fxml/WelcomeController.java
@@ -66,7 +66,7 @@ public final class WelcomeController {
      */
     public void initialize() {
         this.splitPane.setBackground(new Background(new BackgroundImage(
-            new Image(this.getClass().getResource(ResourceManager.IMAGE_WELCOME).toString()),
+            new Image(this.getClass().getResource(ResourceManager.IMAGE_WELCOME).toExternalForm()),
             BackgroundRepeat.NO_REPEAT,
             BackgroundRepeat.NO_REPEAT,
             BackgroundPosition.CENTER,
