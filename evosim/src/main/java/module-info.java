1369f7088da9e60c95723b1efa9523f30e26e765
ptrstr <ptrstr@protonmail.com>
2023-05-12 23:13:39 -0400

    another 1

--- evosim/src/main/java/module-info.java
+++ evosim/src/main/java/module-info.java
@@ -18,6 +18,7 @@ module org.totallyspies.evosim {
     opens org.totallyspies.evosim.utils to com.fasterxml.jackson.databind;
     opens org.totallyspies.evosim.entities to com.fasterxml.jackson.databind;
     opens org.totallyspies.evosim.geometry to com.fasterxml.jackson.databind;
+    opens org.totallyspies.evosim.images;
     exports org.totallyspies.evosim.fxml;
     exports org.totallyspies.evosim.ui;
     exports org.totallyspies.evosim.geometry;
